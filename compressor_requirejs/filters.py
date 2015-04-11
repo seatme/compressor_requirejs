9811516772e1782563e804ac1c61fa47f380611c
Steven DeMartini <sjd@yelp.com>
2015-04-11 14:29:02 -0400

    Use django-compressor tools for finding file paths

--- compressor_requirejs/filters.py
+++ compressor_requirejs/filters.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import os
+import codecs
 
 from compressor.filters.base import FilterBase
-from django.contrib.staticfiles import finders
+from compressor.js import JsCompressor
+from django.conf import settings as django_settings
 
 from compressor_requirejs.compiler import RequireJSCompiler
 from compressor_requirejs.config import settings
@@ -16,41 +17,55 @@ class RequireJSPrecompiler(FilterBase):
         self.filter_type = filter_type
         self.filename = filename
         self.charset = charset
+
+        # The main module to load, which should correspond to the "name" in the
+        # build file
+        self.module_name = self.attrs.get('data-name')
+
+        # The RequireJS compiler for building optimized JS
         self.requireJSCompiler = RequireJSCompiler()
 
+        # Use Django-Compressor's builtin Compressor tools to get the file
+        # paths and contents of needed files
+        self.compressor = JsCompressor()
+
     def input(self, **kwargs):
-        build_filename = self.get_fullpath(kwargs['basename'], resolve_path=True)
+        if getattr(django_settings, 'COMPRESS_ENABLED', False):
+            # Only use built files (from the RequireJS optimizer) when compress
+            # is enabled
+            build_filename = self.get_filepath(kwargs['basename'])
 
-        global_config = getattr(settings, 'COMPRESSOR_REQUIREJS_GLOBAL_CONFIG', None)
-        config_path = self.get_fullpath(global_config)
+            global_config = getattr(settings, 'COMPRESSOR_REQUIREJS_GLOBAL_CONFIG', None)
+            config_path = self.get_filepath(global_config)
 
-        paths = self.get_paths_from_required_libs()
+            paths = self.get_paths_from_required_libs()
 
-        return self.requireJSCompiler.requirejs(
-            build_filename, config_path=config_path, paths=paths)
-
-    def get_fullpath(self, path, resolve_path=True):
-        if os.path.isabs(path):
-            return path
-        if not resolve_path:
-            return path
-        files = finders.find(path, all=True)
-        if isinstance(files, list):
-            if len(files) > 0:
-                return files[0]
-            else:
-                return path
-        elif files is not None:
-            return files
+            return self.requireJSCompiler.requirejs(
+                build_filename, config_path=config_path, paths=paths,
+                charset=self.charset)
         else:
-            return path
+            # Return the content of the main file, with no RequireJS
+            # optimization (i.e., just use async-loading for all modules)
+            module_filepath = self.get_filepath(self.module_name)
+            with codecs.open(module_filepath, 'r', self.charset) as f:
+                return f.read()
 
     def get_paths_from_required_libs(self):
         paths = []
         if hasattr(settings, 'COMPRESSOR_REQUIREJS_REQUIRED_LIBS'):
             for arg in settings.COMPRESSOR_REQUIREJS_REQUIRED_LIBS.keys():
-                path = self.get_fullpath(settings.COMPRESSOR_REQUIREJS_REQUIRED_LIBS[arg])
+                path = self.get_filepath(settings.COMPRESSOR_REQUIREJS_REQUIRED_LIBS[arg])
                 if path.endswith('.js'):
                     path = path[:-3]
                 paths.append('paths.%s=%s' % (arg, path))
         return paths
+
+    def get_filepath(self, static_path):
+        """
+        Get the absolute file-path of the the file given its
+        STATIC_ROOT-relative path.
+
+        Returns the file path on the file system.
+        """
+        basename = self.compressor.get_basename(static_path)
+        return self.compressor.get_filename(basename)
